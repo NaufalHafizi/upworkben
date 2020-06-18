dc67b6be6c23bcd3bd8804b6edeea3682e3d54c9
NaufalHafizi <naufalhafizi@gmail.com>
2020-06-18 23:17:13 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/upworkben2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naufalhafizi.upworkben
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child: WebviewScaffold(
+        url: "https://www.upwork.com/freelancers/~015718a418b559aca7",
+      ),
+    );
+  }
+}
